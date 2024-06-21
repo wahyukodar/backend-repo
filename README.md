77b1ac35d24cb19ad5ae1d66937f53c1ac844520
Wahyu Kodar <wahyukodar@gmail.com>
2024-06-21 13:26:06 +0700

    update port

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 
 ### Informations
 
-- Backend service running on port 3000
+- Backend service running on port 5000
 - [Restclient collections](https://raw.githubusercontent.com/wahyukodar/backend-repo/master/http/users.http)
 
--- core/app.ts
+++ core/app.ts
@@ -3,7 +3,7 @@ import userRoutes from '../routes/userRoutes';
 import { errorHandler } from '../middleware/errorHandler';
 
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 5000;
 
 app.use(express.json());
 app.use('/users', userRoutes);
